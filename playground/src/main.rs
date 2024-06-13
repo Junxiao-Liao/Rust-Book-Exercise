710e2735ff25178e2358bf678d8afc7e1e98ea74
junxiaoliao <huruyuanxingke66@gmail.com>
2024-06-13 15:52:37 +0800

    Chapter: Using Structs to Structure Related Data

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -72,4 +72,28 @@
 //     &s[..]
 // }
 
-fn main() {}
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+
+fn main() {
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(rect1);
+}
