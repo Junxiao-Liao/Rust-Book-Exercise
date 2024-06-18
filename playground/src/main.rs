47463989526c910c89145e8702df2fb51f73af1c
junxiaoliao <huruyuanxingke66@gmail.com>
2024-06-18 17:12:06 +0800

    Chapter: Smart Pointers

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -235,13 +235,124 @@
 //     println!("After calling closure: {list:?}");
 // }
 
-use std::thread;
+// use std::thread;
+
+// fn main() {
+//     let list = vec![1, 2, 3];
+//     println!("Before defining closure: {list:?}");
+
+//     thread::spawn(move || println!("From thread: {list:?}"))
+//         .join()
+//         .unwrap();
+// }
+
+// fn main() {
+//     let b = Box::new(5);
+//     println!("b = {b}");
+// }
+
+// #[derive(Debug)]
+// enum List<T> {
+//     Cons(T, Box<List<T>>),
+//     Nil,
+// }
+
+// use crate::List::{Cons, Nil};
+
+// fn main() {
+//     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+//     println!("{:?}", list);
+// }
+
+// struct MyBox<T>(T);
+
+// impl<T> MyBox<T> {
+//     fn new(x: T) -> MyBox<T> {
+//         MyBox(x)
+//     }
+// }
+
+// use std::ops::Deref;
+
+// impl<T> Deref for MyBox<T> {
+//     type Target = T;
+
+//     fn deref(&self) -> &Self::Target {
+//         &self.0
+//     }
+// }
+
+// fn hello(name: &str) {
+//     println!("Hello, {name}!");
+// }
+
+// fn main() {
+//     let m = MyBox::new(String::from("Rust"));
+//     hello(&m);
+// }
+
+// struct CustomSmartPointer {
+//     data: String,
+// }
+
+// impl Drop for CustomSmartPointer {
+//     fn drop(&mut self) {
+//         println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+//     }
+// }
+
+// fn main() {
+//     let c = CustomSmartPointer {
+//         data: String::from("my stuff"),
+//     };
+//     let d = CustomSmartPointer {
+//         data: String::from("other stuff"),
+//     };
+//     drop(c);
+//     println!("CustomSmartPointers created.");
+// }
+
+// enum List {
+//     Cons(i32, Rc<List>),
+//     Nil,
+// }
+
+// use crate::List::{Cons, Nil};
+// use std::rc::Rc;
+
+// fn main() {
+//     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+//     println!("count after creating a = {}", Rc::strong_count(&a));
+//     let b = Cons(3, Rc::clone(&a));
+//     println!("count after creating b = {}", Rc::strong_count(&a));
+//     {
+//         let c = Cons(4, Rc::clone(&a));
+//         println!("count after creating c = {}", Rc::strong_count(&a));
+//     }
+//     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+// }
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
 
 fn main() {
-    let list = vec![1, 2, 3];
-    println!("Before defining closure: {list:?}");
+    let value = Rc::new(RefCell::new(5));
 
-    thread::spawn(move || println!("From thread: {list:?}"))
-        .join()
-        .unwrap();
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
 }
